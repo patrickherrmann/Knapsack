6da935831c4c455db7fa6125974f8c151597279f
Patrick Herrmann <patrickwherrmann@gmail.com>
2013-05-23 09:57:11 -0700

    Add Item class

---
+++ Item.java
@@ -0,0 +1,11 @@
+public class Item {
+
+   public int label;
+   public double value;
+   public double weight;
+   
+   public Item(int label, double value, double weight) {
+      this.value = value;
+      this.weight = weight;
+   }
+}
